1eaccf113808be553f2d67875eb6499402026292
Nick Speal <nick.speal+github@flyzipline.com>
2021-11-06 16:04:09 -0700

    simplify appConfig combining

--- src/appConfig/index.js
+++ src/appConfig/index.js
@@ -1,14 +1,14 @@
-import defaults from 'appConfig/default.js';
-import sf from 'appConfig/sf.js';
-import tahoe from 'appConfig/tahoe.js';
+import defaults from 'appConfig/default';
+import sf from 'appConfig/sf';
+import tahoe from 'appConfig/tahoe';
 
-const region = process.env.NEXT_PUBLIC_REGION;
-
-const combined_config = { ...defaults };
-if (region === 'sf') {
-  Object.assign(combined_config, sf);
-} else if (region === 'tahoe') {
-  Object.assign(combined_config, tahoe);
+const configs = {
+  defaults,
+  sf,
+  tahoe,
 }
 
-export default combined_config;
+const region = process.env.NEXT_PUBLIC_REGION;
+const config = { ...configs.defaults, ...configs[region] };
+
+export default config;
