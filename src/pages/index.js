2a66ac2b1742ff36caff885b6f936d357334f2ee
Robert Nealan <rob@atomidesign.com>
2021-11-28 23:17:41 -0800

    Consolidate Head repeats into _app.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,6 @@ import {
 
 import appConfig from 'appConfig';
 
-const ELEVATION_HEIGHT = 175;
-
 const IndexPage = () => {
   const dispatch = useDispatch();
   const startAddress = useSelector((state) => state.search.startAddress);
@@ -200,50 +198,8 @@ const IndexPage = () => {
     <>
       <Head>
         <title>{appConfig.PAGE_TITLE}</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-          key="viewport"
-        />
-        <meta
-          name="description"
-          content="Avoid hills and find bike routes with Bikesy.com, an innovative and open-source bike mapping system specially designed to find flat, safe, and fast bike routes anywhere in the San Francisco Bay Area. San Francisco isn’t New York – they might have taller buildings, but we’ve got bigger hills. Since we couldn’t find another service that lets you choose slightly longer but less steep routes, we made our own. Even better, Bikesy automatically gives you an elevation profile for your ride to help you prepare for the tough parts."
-        />
-
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css"
-          rel="stylesheet"
-        />
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="favicon/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="favicon/favicon-16x16.png"
-          sizes="16x16"
-        />
-        <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="theme-color" content="#ffffff" />
-
-        <script src="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.js"></script>
       </Head>
+
       <div>
         <TitleBar changeMobileView={changeMobileView} mobileView={mobileView} />
 
@@ -255,18 +211,15 @@ const IndexPage = () => {
           updateControls={updateControls}
         />
 
-        <Directions height={directionsHeight} mobileView={mobileView} />
+        <Directions mobileView={mobileView} />
 
         <Map
           assignStartLocation={assignStartLocation}
           assignEndLocation={assignEndLocation}
-          height={mapHeight}
           mobileView={mobileView}
         />
 
         <Elevation
-          width={elevationWidth}
-          height={ELEVATION_HEIGHT}
           toggleElevationVisibility={toggleElevationVisibility}
           elevationVisible={elevationVisible && Boolean(elevationProfile)}
           mobileView={mobileView}
